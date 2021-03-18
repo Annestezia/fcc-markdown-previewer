c893342fa001973850bee0bf6e17b45bfd69efff
Annestezia <cast.wheel@gmail.com>
2021-03-19 02:12:27 +1000

    ++ FCC tests, cleanup, default text to separate file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import marked from 'marked';
+import defaultContent from './defaultContent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {  
+  constructor(props) {
+    super(props);    
+    this.state = {
+      value: defaultContent};
+    this.handleChange = this.handleChange.bind(this);
+    this.createMarkup = this.createMarkup.bind(this);
+  }
+  
+componentDidMount() {}
+  handleChange(e) {
+    this.setState({
+      value: e.target.value 
+    });
+  }   
+  createMarkup(str) {
+    marked.setOptions({breaks: true});
+    return {
+      __html: marked(str)
+    };
+  }
+  render(){
+    return (<div class = "wrap">
+          <div class = "app_title"><h1>Markdown Previewer</h1></div>
+          <Editor value= {this.state.value} onChange ={this.handleChange}/>
+          <Preview  markup = {this.createMarkup(this.state.value)}/>
+          <footer>by Annestezia</footer>
+  </div>
+    );
+  }
 }
+
+const Editor = (props)=> {
+    return (
+      <textarea id="editor" classNames = "column" name="editor" cols="1" rows="15" onChange = {props.onChange}>{props.value}</textarea>
+    );
+};
+
+const Preview = (props)=>{
+  return <div id = "preview" className = "column" dangerouslySetInnerHTML={props.markup} />;
+};
 
 export default App;
---
+++ src/defaultContent.js
@@ -0,0 +1,63 @@
+const defaultContent = `# Ok, heres markup example \`h1\`
+## Let me tell you a bit about the previewer \`h2\`
+> You can see 
+- editor window (you can change text or delete completely and write your own stuff) and 
+- compiled version (you can see result there).
+Links
+--
+You can create a link like this
+[Google](https://google.com)
+or a re-usable thing 
+
+[Marked]: https://github.com/markedjs/marked/
+
+Now  everytime you write [marked]  in square brackets, you get the link, kinda cool huh?)
+
+Markdown style for   h1
+==
+and h2 , pick  you  fav  =)
+--
+
+_List name in italic github style_
+- item 1
+- **bold item 2**
+- item 3
+
+
+## \`About code\`
+\`alert('Hola'); \\\\ inline code\`
+
+\`\`\`
+this
+is a code block
+()=>{}
+\`\`\`
+
+
+<pre>mycode and evenmore code
+and  on another line yep</pre>
+And an _image_ , just like a _link_ but with an exclamation mark in front
+
+![img](http://personal.psu.edu/xqz5228/jpg.jpg)
+
+>> ***W. Shakespeare***
+
+> <pre>When most I wink, then do mine eyes best see,
+For all the day they view things unrespected;
+But when I sleep, in dreams they look on thee,
+And darkly bright are bright in dark directed.
+Then thou, whose shadow shadows doth make bright,
+How would thy shadow's form form happy show
+To the clear day with thy much clearer light,
+When to unseeing eyes thy shade shines so!
+How would, I say, mine eyes be blessed made
+By looking on thee in the living day, 
+When in dead night thy fair imperfect shade
+Through heavy sleep on sightless eyes doth stay!
+All days are nights to see till I see thee,
+And nights bright days when dreams do show thee me.</pre><br />
+
+And  heres is [marked.js docs](https://marked.js.org/) in case you want more details.
+`;
+
+export default defaultContent;
